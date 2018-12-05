72417f15fac2a90e7f8f76e5a022b65e74669e1b
lmbullard <40184221+lmbullard@users.noreply.github.com>
2018-12-05 15:27:25 -0500

    updated settings Activity

--- app/src/main/java/edu/valdosta/workoutapp/SettingsActivity.java
+++ app/src/main/java/edu/valdosta/workoutapp/SettingsActivity.java
@@ -1,13 +1,21 @@
 package edu.valdosta.workoutapp;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    Button createProfile;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        createProfile = (Button) findViewById(R.id.createProfile);
     }
-}
+    public void createProfileOnClick(View view){
+        startActivity(new Intent(SettingsActivity.this, ProfileActivity.class));
+    }
+
