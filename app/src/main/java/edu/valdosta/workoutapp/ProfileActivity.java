1fdf0ff20a16a5bf3f2f83409799daf61ccfef81
lmbullard <40184221+lmbullard@users.noreply.github.com>
2018-12-05 17:39:50 -0500

    update 2.0

--- app/src/main/java/edu/valdosta/workoutapp/ProfileActivity.java
+++ app/src/main/java/edu/valdosta/workoutapp/ProfileActivity.java
@@ -10,6 +10,7 @@ public class ProfileActivity extends Activity{
     int heightInInches;
     int bmi;
     int weight;
+    int skill;
     String sex;
     String name;
     EditText nameView;
@@ -17,21 +18,34 @@ public class ProfileActivity extends Activity{
     EditText heightView;
     RadioButton maleRadio;
     RadioButton femaleRadio;
-
+    RadioButton weak;
+    RadioButton neutral;
+    RadioButton athletic;
+    RadioButton novice;
+    RadioButton intermediate;
+    RadioButton expert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         maleRadio = (RadioButton) findViewById(R.id.maleRadio);
         femaleRadio = (RadioButton) findViewById(R.id.femaleRadio);
+        weak = (RadioButton) findViewById(R.id.weak);
+        neutral = (RadioButton) findViewById(R.id.neutral);
+        athletic = (RadioButton) findViewById(R.id.athletic);
+        novice = (RadioButton) findViewById(R.id.novice);
+        intermediate = (RadioButton) findViewById(R.id.intermediate);
+        expert = (RadioButton) findViewById(R.id.expert);
         nameView = (EditText) findViewById(R.id.nameView);
         ageView = (EditText) findViewById(R.id.ageView);
         heightView = (EditText) findViewById(R.id.heightView);
+        skill = 0;
         age = 0;
         heightInInches = 0;
         bmi = 0;
         weight = 0;
     }
+
     public int calculateBMI(int heightInInches, int weight){
         bmi= 703 * weight/(heightInInches^2);
         return bmi;
@@ -43,9 +57,29 @@ public class ProfileActivity extends Activity{
         else{
             sex= "Female";
         }
+        if(weak.isChecked()){
+            skill += 1;
+        }
+        else if (neutral.isChecked()){
+            skill += 2;
+        }
+        else{
+            skill += 3;
+        }
+        if(novice.isChecked()){
+            skill += 1;
+        }
+        else if (intermediate.isChecked()){
+            skill += 2;
+        }
+        else{
+            skill += 3;
+        }
+
         name = nameView.getText().toString();
         age = Integer.parseInt(ageView.getText().toString());
         heightInInches = Integer.parseInt(heightView.getText().toString());;
 
     }
 }
+
